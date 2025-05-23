4444023c0a76435510d55b0f216c5da936e3ac0f
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-05-23 00:48:02 -0400

    DataGeneretors class set up with required fields

--- src/main/java/com/ryan/pastamod/datagen/DataGenerators.java
+++ src/main/java/com/ryan/pastamod/datagen/DataGenerators.java
@@ -1,5 +1,25 @@
 package com.ryan.pastamod.datagen;
 
+import java.util.concurrent.CompletableFuture;
+
+import com.ryan.pastamod.PastaMod;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = PastaMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event){
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+    }
 
 }
