182b97cbd19b33f1bfdb266e1a27ba0c1ce6b2d6
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-05-24 22:26:38 -0400

    Implemented Block state provider for cubeAll mob blocks

--- src/main/java/com/ryan/pastamod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/ryan/pastamod/datagen/ModBlockStateProvider.java
@@ -1,5 +1,35 @@
 package com.ryan.pastamod.datagen;
 
-public class ModBlockStateProvider {
+import com.ryan.pastamod.PastaMod;
+import com.ryan.pastamod.blocks.ModBlocks;
 
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, PastaMod.MODID, exFileHelper);
+    }
+
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.RAINBOW_GLASS_BLOCK);
+        blockWithItem(ModBlocks.AURORA_GLASS_BLOCK);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+
+    // public BlockStateProviderType type() {
+    //     // Return a default BlockState or implement your logic here
+    //     throw new UnsupportedOperationException("Not implemented yet");
+    // }
+
+    // public BlockState getState(RandomSource source, BlockPos pos) {
+    //     // Return a default BlockState or implement your logic here
+    //     throw new UnsupportedOperationException("Not implemented yet");
+    // }
 }
