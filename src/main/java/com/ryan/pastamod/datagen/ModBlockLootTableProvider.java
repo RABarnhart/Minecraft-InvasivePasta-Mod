98c0c9e1f9f04a667b6ad4099f09d9b4391692a9
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-05-25 12:52:24 -0400

    added block drops to loot table generation

--- src/main/java/com/ryan/pastamod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/com/ryan/pastamod/datagen/ModBlockLootTableProvider.java
@@ -1,5 +1,33 @@
 package com.ryan.pastamod.datagen;
 
-public class ModBlockLootTableProvider {
+import java.util.Set;
 
+import com.ryan.pastamod.blocks.ModBlocks;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider{
+    protected ModBlockLootTableProvider(HolderLookup.Provider pRegistries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), pRegistries);
+    }
+
+    @Override
+    protected void generate() {
+        dropWhenSilkTouch(ModBlocks.RAINBOW_GLASS_BLOCK.get());
+        dropWhenSilkTouch(ModBlocks.AURORA_GLASS_BLOCK.get());
+        this.add(Blocks.BOOKSHELF, p_250379_ -> this.createSingleItemTableWithSilkTouch(p_250379_, Items.BOOK, ConstantValue.exactly(3.0F)));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        // making all the blocks defined by the mod available to this class in the way it wants
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
 }
