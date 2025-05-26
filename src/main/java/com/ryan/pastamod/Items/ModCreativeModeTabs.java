e24395cbffb27ed47406ca73f591eb432d99d5f0
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-05-26 14:12:59 -0400

    added a creative tab for the new blocks and the glass block

--- src/main/java/com/ryan/pastamod/Items/ModCreativeModeTabs.java
+++ src/main/java/com/ryan/pastamod/Items/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import com.ryan.pastamod.PastaMod;
 import com.ryan.pastamod.blocks.ModBlocks;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.*;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,7 +18,7 @@ public class ModCreativeModeTabs {
 
     public static final RegistryObject<CreativeModeTab> ALL_MOD_ITEMS_TAB = CREATIVE_MODE_TABS.register("pasta_mod_items_tab",
         () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.RAINBOW_BOOKSHELF_BLOCK.get()))
-        .title(Component.translatable("creativetab.pastamod.pastamod_itmes"))
+        .title(Component.translatable("creativetab.pastamod.pastamod_items_tab"))
         .displayItems((itemDisplayParameters, output) -> {
             output.accept(ModBlocks.RAINBOW_BOOKSHELF_BLOCK.get());
             output.accept(ModBlocks.AURORA_GLASS_BLOCK.get());
@@ -26,6 +27,22 @@ public class ModCreativeModeTabs {
             output.accept(ModItems.STRAWBERRY_LEMON_POPPI.get());
             output.accept(ModItems.ORANGE_POPPI.get());
             output.accept(ModItems.RASPBERRY_ROSE_POPPI.get());
+        })
+        .build());
+
+            public static final RegistryObject<CreativeModeTab> GLASS_BLOCKS_TAB = CREATIVE_MODE_TABS.register("glass_blocks_tab",
+        () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.RAINBOW_BOOKSHELF_BLOCK.get()))
+        .title(Component.translatable("creativetab.pastamod.glass_blocks_tab"))
+        .displayItems((itemDisplayParameters, output) -> {
+            output.accept(Blocks.GLASS);
+            output.accept(ModBlocks.GLASS_SLAB.get());
+            output.accept(ModBlocks.GLASS_STAIRS.get());
+            output.accept(ModBlocks.GLASS_WALL.get());
+            output.accept(ModBlocks.GLASS_FENCE_GATE.get());
+            output.accept(ModBlocks.GLASS_DOOR.get());
+            output.accept(ModBlocks.GLASS_TRAPDOOR.get());
+            output.accept(ModBlocks.GLASS_BUTTON.get());
+            output.accept(ModBlocks.GLASS_PRESSURE_PLATE.get());
         })
         .build());
 
