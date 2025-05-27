d6715a7d713c3219e4f6b6a1f4efe15e0a04c2d8
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-05-26 22:25:13 -0400

    implemented ModBlockTagProvider to generate the walls tag for glass walls so that they connect to each other

--- src/main/java/com/ryan/pastamod/datagen/DataGenerators.java
+++ src/main/java/com/ryan/pastamod/datagen/DataGenerators.java
@@ -30,6 +30,7 @@ public class DataGenerators {
 
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+        generator.addProvider(event.includeClient(), new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper));
     }
 
 }
--- src/main/java/com/ryan/pastamod/datagen/ModBlockTagProvider.java
+++ src/main/java/com/ryan/pastamod/datagen/ModBlockTagProvider.java
@@ -1,5 +1,23 @@
 package com.ryan.pastamod.datagen;
 
-public class ModBlockTagProvider {
+import java.util.concurrent.CompletableFuture;
 
+import com.ryan.pastamod.PastaMod;
+import com.ryan.pastamod.blocks.ModBlocks;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockTagProvider extends BlockTagsProvider{
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper exFileHelper) {
+        super(output, lookupProvider, PastaMod.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(BlockTags.WALLS).add(ModBlocks.GLASS_WALL.get());
+    }
 }
