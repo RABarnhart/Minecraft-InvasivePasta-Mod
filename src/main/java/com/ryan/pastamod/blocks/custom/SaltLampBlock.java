ca663000633d2fee2e1ff14beb81ea0fce0c4d6e
RABarnhart <65832406+RABarnhart@users.noreply.github.com>
2025-06-05 11:01:02 -0400

    Created new class for the salt lamp block

---
+++ src/main/java/com/ryan/pastamod/blocks/custom/SaltLampBlock.java
@@ -0,0 +1,12 @@
+package com.ryan.pastamod.blocks.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class SaltLampBlock extends Block {
+
+
+    
+    public SaltLampBlock(Properties properties) {
+        super(properties);
+    }
+}
